570a4729948dc8f3ddbd26297b9db005f92a1106
Suren Melyan <surmel9@gmail.com>
2023-09-29 00:12:57 +0400

    restaurants screen ready

---
+++ src/components/utility/safe-area.component.js
@@ -0,0 +1,13 @@
+import styled from "styled-components/native";
+import {SafeAreaProvider, SafeAreaView} from "react-native-safe-area-context";
+import {StatusBar} from "react-native";
+
+export const SafetyProvider = styled(SafeAreaProvider)`
+  background-color: ${(props) => props.theme.colors.bg.secondary};
+`;
+
+export const SaveArea = styled(SafeAreaView)`
+  flex: 1;
+  margin-top: ${StatusBar.currentHeight}px;
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
---
+++ src/features/restaurants/screens/map.screen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+export const MapsScreen = () => {
+  return (
+    <View>
+      <Text>Maps screen!!!</Text>
+    </View>
+  );
+};
